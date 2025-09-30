b12352e23be796e2f96320d4800f6630ae65ef29
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2025-09-30 15:49:16 +0300

    MauiGRUD
    Uue projekti valmimine

--- MauiCRUD/MauiCRUD/AppShell.xaml.cs
+++ MauiCRUD/MauiCRUD/AppShell.xaml.cs
@@ -1,45 +1,10 @@
-﻿#if WINDOWS
-using Microsoft.UI;
-using Microsoft.UI.Windowing;
-using Windows.Graphics;
-#endif
-
-
-namespace MauiCRUD
+﻿namespace MauiCRUD
 {
-    public partial class App : Application
+    public partial class AppShell : Shell
     {
-
-        const int WindowWidth = 540;
-        const int WindowHeight = 1200;
-
-        public App()
+        public AppShell()
         {
             InitializeComponent();
-
-            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
-            {
-            #if WINDOWS
-			var mauiWindow = handler.VirtualView;
-			var nativeWindow = handler.PlatformView;
-			nativeWindow.Activate();
-			IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
-			WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
-			AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-			appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
-            #endif
-            });
-
-            MainPage = new AppShell();
         }
-
-        //protected override Window CreateWindow(IActivationState? activationState)
-        //{
-        //    return new Window(new AppShell());
-        //}
     }
 }
-
-
-
-
---
+++ MauiCRUD/MauiCRUD/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiCRUD.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Product Clone() => MemberwiseClone() as Product;
+        public (bool isValid, string? ErrorMessage) Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                return (false, $"{nameof(Name)} is required");
+            }
+            else if (Price <= 0) 
+            { 
+                return (false, $"{nameof(Price)} should be grater than 0");
+            }
+            return (true, null);
+        }
+    }
+}
